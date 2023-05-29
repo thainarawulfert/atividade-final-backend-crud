0942108c476979e9b268c772882d5936eb6ed187
Thainara Wulfert <thainarawulfertoliveira@gmail.com>
2023-05-28 21:06:42 -0300

    finalmente funcional

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 let usuarios = [];
+
+const validarUsuario = (req, res, next) => {
+  const id = Number(req.params.id);
+  const buscarId = usuarios.findIndex(
+    (buscarUsuario) => buscarUsuario.id === id
+  );
+  if (buscarId === -1) {
+    return res.status(400).json("Usuário não localizado!");
+  } else {
+    next();
+  }
+};
+
+const validarRecado = (req, res, next) => {
+  const idRecado = Number(req.params.idRecados);
+  const idPessoa = Number(req.params.id);
+
+  const indexPessoa = usuarios.findIndex((p) => p.id === idPessoa);
+
+  const indexRecado = usuarios[indexPessoa].recados.findIndex(
+    (r) => r.idRecados === idRecado
+  );
+
+  if (indexRecado === -1) {
+    return res.status(400).json("Recado não encontrado");
+  }
+
+  res.status(207).json("Recado alterado");
+  next();
+};
 
 //Fazer o registro do usuário no CRUD
 
@@ -60,17 +100,17 @@ app.get("/usuarios", (req, res) => {
 
 // Criação de Recado
 
-app.post("/usuarios/:id/recados", (req, res) => {
+app.post("/usuarios/:id/recados", validarUsuario, (req, res) => {
   const { titulo, mensagem } = req.body;
   const idRecados = Math.floor(Math.random() * 1000000);
   const userId = parseInt(req.params.id);
   const buscarUsuario = usuarios.find((users) => users.id === userId);
 
-  if (!buscarUsuario) {
-    return res.status(401).send("Usuário não encontrado ou não existe");
-  }
-
-  const newRecado = { id: idRecados, titulo: titulo, mensagem: mensagem };
+  const newRecado = {
+    idRecados: idRecados,
+    titulo: titulo,
+    mensagem: mensagem,
+  };
 
   buscarUsuario.recados.push(newRecado);
 
@@ -78,44 +118,45 @@ app.post("/usuarios/:id/recados", (req, res) => {
 });
 
 // Edição de Recado
-app.put("/usuarios/:id/recados/:idEditar", (req, res) => {
-  const { titulo, mensagem } = req.body;
-  const idUsuario = parseInt(req.params.id);
-  const buscarUsuario = usuarios.find((users) => users.id === idUsuario);
-  const idEditar = parseInt(req.params.idEditar);
-  const recadoEditado = buscarUsuario.recados.findIndex(
-    (recadosEditar) => recadosEditar.id === idEditar
-  );
+app.put(
+  "/usuarios/:id/recados/:idRecados",
+  validarUsuario,
+  validarRecado,
+  (req, res) => {
+    const id = Number(req.params.id);
+    const idRecado = Number(req.params.idRecados);
+    const indexPessoa = usuarios.findIndex((p) => p.id === id);
 
-  if (!idUsuario || !idEditar) {
-    return res
-      .status(401)
-      .send("Usuário ou Recado não encontrado ou não existe");
+    const indexRecado = usuarios[indexPessoa].recados.findIndex(
+      (r) => r.idRecados === idRecado
+    );
+
+    const recados = req.body;
+
+    usuarios[indexPessoa].recados[indexRecado] = {
+      idRecados: (recados.idRecados = idRecado),
+      titulo: recados.titulo,
+      descricao: recados.descricao,
+    };
   }
-
-  buscarUsuario.recados[recadoEditado].titulo = titulo;
-  buscarUsuario.recados[recadoEditado].mensagem = mensagem;
-
-  res.status(200).send("Recado editado com sucesso!!");
-});
+);
 
 // Deletar Recados
-app.delete("/usuarios/:id/recados/:idEditar/excluir", (req, res) => {
-  const idUsuario = parseInt(req.params.id);
-  const buscarUsuario = usuarios.find((users) => users.id === idUsuario);
-  const idEditar = parseInt(req.params.idEditar);
-  const indexRecado = buscarUsuario.recados.findIndex(
-    (recadosEditar) => recadosEditar.id === idEditar
-  );
+app.delete(
+  "/usuarios/:id/recados/:idRecados/excluir",
+  validarUsuario,
+  validarRecado,
+  (req, res) => {
+    const id = Number(req.params.id);
+    const idRecado = Number(req.params.idRecados);
+    const indexPessoa = usuarios.findIndex((p) => p.id === id);
 
-  if (indexRecado > 0) {
-    return res.status(401).send("Recado não encontrado ou não existe");
+    const indexRecado = usuarios[indexPessoa].recados.findIndex(
+      (r) => r.idRecados === idRecado
+    );
+
+    usuarios[indexPessoa].recados.splice(indexRecado, 1);
   }
-
-  buscarUsuario.recados = buscarUsuario.recados.filter(
-    (recado) => recado.id !== idEditar
-  );
-  res.status(200).send("Recado excluido com sucesso!!");
-});
+);
 
 app.listen(8080, () => console.log("Servidor iniciado...."));
